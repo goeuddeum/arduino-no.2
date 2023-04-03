34d368edf1ea8ecbada75d2e0199fe8fa53faa9a
goeuddeum <125548154+goeuddeum@users.noreply.github.com>
2023-04-03 13:09:22 +0900

    Add files via upload

---
+++ millis_9/millis_9.ino
@@ -0,0 +1,40 @@
+const int t2_LED=11;
+unsigned int t_high = 0;
+
+const int t1_LED=13;
+unsigned int on_off = 0;
+
+unsigned long t1_prev=0;
+const unsigned long t1_delay=500;
+
+unsigned long t2_prev=0;
+const unsigned long t2_delay=4;
+const int buttonPin=10;
+
+void setup() {
+  pinMode(t1_LED,OUTPUT);
+  pinMode(t2_LED,OUTPUT);
+  pinMode(buttonPin,INPUT);
+}
+
+void loop() {
+  int buttonInput=digitalRead(buttonPin);
+  if(buttonInput==HIGH){
+  unsigned long t1_now=millis();
+  if(t1_now-t1_prev >= t1_delay){
+    t1_prev = t1_now;
+    
+    on_off++;
+    if(on_off>1) on_off = 0;
+  digitalWrite(t1_LED,on_off);   
+}
+  unsigned long t2_now=millis();
+  if(t2_now-t2_prev >= t2_delay){
+    t2_prev = t2_now;
+    
+    t_high++;
+    if(t_high>255) t_high = 0;
+  analogWrite(t2_LED,t_high);   
+}
+  }
+}
